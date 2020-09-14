d0335ffb96fabf81d70190693d756a0eb3784393
Brian Moriguchi <brian@beckos.com>
2020-09-14 16:14:59 -0700

    Added UI properties and icons (Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> )

--- CoreMotion_Sample01/ViewController.swift
+++ CoreMotion_Sample01/ViewController.swift
@@ -12,19 +12,45 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelX: UILabel!
+    @IBOutlet weak var labelY: UILabel!
+    @IBOutlet weak var labelZ: UILabel!
+    @IBOutlet weak var intervalSlider: UISlider!
+    @IBOutlet weak var intervalLabel: UILabel!
+    
     let motion = CMMotionManager()
     var timer = Timer()
-  
+    var intervalN = 1.0
+   
+    @IBAction func sliderValueChange(_ sender: Any) {
+        
+        intervalN = Double(intervalSlider.value * 60.0)
+        intervalLabel.text = "\(intervalN)"
+        //intervalN = intervalN
+    }
+    @IBAction func updateButton(_ sender: Any) {
+        
+        getAccelerometerData(sliderValue: intervalN)
+    }
     
+    @IBAction func stopButton(_ sender: UIButton) {
+        motion.stopAccelerometerUpdates()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        intervalLabel.text = "1.0"
+        intervalSlider.value = 1.0/60.0
+  
+    }
 
-        
+
+    func getAccelerometerData(sliderValue: Double) {
         // Check to see if the accelerometer hardware is available.
         if self.motion.isAccelerometerAvailable {
-            self.motion.accelerometerUpdateInterval = 1.0/60.0 // 60 Hz update frequency
+            self.motion.accelerometerUpdateInterval = sliderValue/60.0 // 60 Hz update frequency
             self.motion.startAccelerometerUpdates()
-
+            
             // Configure a timer to fetchthe data.
             self.timer = Timer(fire: Date(), interval: (1.0/60.0), repeats: true, block: {(timer) in
                 // Get the accelerometer data.
@@ -32,25 +58,21 @@ class ViewController: UIViewController {
                     let x = data.acceleration.x
                     let y = data.acceleration.y
                     let z = data.acceleration.z
-
+                    
                     // Display the accelerometer data on UI.
+                    self.labelX.text = String(x)
+                    self.labelY.text = String(y)
+                    self.labelZ.text = String(z)
                     
-                    print("x: \(x)")
-                    print("y: \(y)")
-                    print("z: \(z)")
-                
-                    
-
-
                 }
             })
-
+            
             // Add the timer to the current run loop.
             RunLoop.current.add(self.timer, forMode: .default)
         }
         
     }
-
-
+    
+    
 }
 
