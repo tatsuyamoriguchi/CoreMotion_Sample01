27c956b47dff3d5d3916a4a3d4926e5eae516694
Brian Moriguchi <brian@beckos.com>
2020-09-14 13:56:37 -0700

    First commit

--- CoreMotion_Sample01/ViewController.swift
+++ CoreMotion_Sample01/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import CoreMotion
+
 
 class ViewController: UIViewController {
 
+    let motion = CMMotionManager()
+    var timer = Timer()
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        // Check to see if the accelerometer hardware is available.
+        if self.motion.isAccelerometerAvailable {
+            self.motion.accelerometerUpdateInterval = 1.0/60.0 // 60 Hz update frequency
+            self.motion.startAccelerometerUpdates()
+
+            // Configure a timer to fetchthe data.
+            self.timer = Timer(fire: Date(), interval: (1.0/60.0), repeats: true, block: {(timer) in
+                // Get the accelerometer data.
+                if let data = self.motion.accelerometerData {
+                    let x = data.acceleration.x
+                    let y = data.acceleration.y
+                    let z = data.acceleration.z
+
+                    // Display the accelerometer data on UI.
+                    
+                    print("x: \(x)")
+                    print("y: \(y)")
+                    print("z: \(z)")
+                
+                    
+
+
+                }
+            })
+
+            // Add the timer to the current run loop.
+            RunLoop.current.add(self.timer, forMode: .default)
+        }
+        
     }
 
 
